28167757beb13663c0e7756851b57695e3acfecf
Irina Kagan <irinagamb@gmail.com>
2019-10-01 21:53:40 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
 
 // Write your code here
+
+var newID = 1000000000 + oldID
+
+
+var ageIsValid = false
+
+if(Number.isInteger(currentAge)){
+  ageIsValid = true
+}else{
+  ageIsValid = false
+}
+
+var randomNumber = Math.random()
+var randomInteger =0 //
+
+var randomUserID = 1000000001 +  Math.floor(randomNumber * 20) + 1;
