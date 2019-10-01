796d979b9b690d6b6debbfe82d22002a37adf24d
Irina Kagan <irinagamb@gmail.com>
2019-10-01 22:08:01 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,18 +3,8 @@
 
 var num1 = 31;
 var num2 = 2;
-num1 * num2;
+var mutiply = num1 * num2;
 
-
-var ageIsValid = false
-
-if(Number.isInteger(currentAge)){
-  ageIsValid = true
-}else{
-  ageIsValid = false
-}
 
 var randomNumber = Math.random()
-var randomInteger =0 //
 
-var randomUserID = 1000000001 +  Math.floor(randomNumber * 20) + 1;
